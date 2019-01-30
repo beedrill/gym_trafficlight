e9f865b1da09c30feabe45aa90182cf93a44a59e
Rusheng <zrs1990@gmail.com>
2019-01-30 16:53:01 -0500

    added 3 eval environment

--- gym_trafficlight/__init__.py
+++ gym_trafficlight/__init__.py
@@ -81,5 +81,39 @@ register(
     nondeterministic = True,
     kwargs = args,
 )
+
+args = {}
+args['traffic_light_module'] = TrafficLightLuxembourg
+args['map_file'] = '1-intersection-Lust-12408/detailed/traffic.net.xml'
+args['route_file'] = '1-intersection-Lust-12408/detailed/traffic-2.rou.xml'
+args['num_traffic_state'] = 26
+
+register(
+    id='TrafficLight-Lust12408-midnight-eval-v0',
+    entry_point='gym_trafficlight.trafficenvs:TrafficEnv',
+    timestep_limit=3000,
+    nondeterministic = True,
+    kwargs = args,
+)
+
+args['route_file'] = '1-intersection-Lust-12408/detailed/traffic-8.rou.xml'
+
+register(
+    id='TrafficLight-Lust12408-rush-hour-eval-v0',
+    entry_point='gym_trafficlight.trafficenvs:TrafficEnv',
+    timestep_limit=3000,
+    nondeterministic = True,
+    kwargs = args,
+)
+
+args['route_file'] = '1-intersection-Lust-12408/detailed/traffic-14.rou.xml'
+
+register(
+    id='TrafficLight-Lust12408-regular-time-eval-v0',
+    entry_point='gym_trafficlight.trafficenvs:TrafficEnv',
+    timestep_limit=3000,
+    nondeterministic = True,
+    kwargs = args,
+)
 #print('registered to gym env')
 #TODO add different environments as a benchmark
